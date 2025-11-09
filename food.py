73bc9dcb4f2481705eb52527094886029013d928
Krishna V <137983314+dewathslayer@users.noreply.github.com>
2025-11-09 23:12:55 +0530

    Add files via upload

---
+++ food.py
@@ -0,0 +1,18 @@
+# food.py
+from turtle import Turtle
+import random
+
+class Food(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)  # smaller dot
+        self.color("red")
+        self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
+        x = random.randint(-14, 14) * 20
+        y = random.randint(-14, 14) * 20
+        self.goto(x, y)
